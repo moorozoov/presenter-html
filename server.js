f0df2b94d8b97019297f6d7cc3f38b480999ff80
morozik <moorozoov@yandex.ru>
2022-04-09 11:30:32 +0300

    body-parser added

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 const path = require('path');
 const router = express.Router();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); 
 
 
-router.get('/', function (req, res) {
+router.get('/editor', function (req, res) {
     res.sendFile(path.join(__dirname + '/main.html'));
 });
+
+router.get('/auth', function (req, res) {
+    res.sendFile(path.join(__dirname + '/login.html'));
+});
+
+//router.post('/')
 
 app.use('/', router);
 app.use(express.static(__dirname));
